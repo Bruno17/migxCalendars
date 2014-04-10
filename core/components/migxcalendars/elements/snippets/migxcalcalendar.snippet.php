e95e31a2f75c5c81a669745a39af8ff6867888c9
Bruno17 <b.perner@gmx.de>
2014-04-10 11:16:52 +0200

    detail-page and browser-history

--- core/components/migxcalendars/elements/snippets/migxcalcalendar.snippet.php
+++ core/components/migxcalendars/elements/snippets/migxcalcalendar.snippet.php
@@ -1,36 +1,96 @@
-$lang = $modx->getOption('lang',$scriptProperties,$modx->getOption('cultureKey'));
-$editable = $modx->getOption('editable',$scriptProperties,'false');
-$aspectRatio = (float) $modx->getOption('aspectRatio',$scriptProperties,'0.4');
-$minTime = $modx->getOption('minTime',$scriptProperties,'6:00');
-$defaultView = $modx->getOption('defaultView',$scriptProperties,'agendaWeek');
+$lang = $modx->getOption('lang', $scriptProperties, $modx->getOption('cultureKey'));
+$editable = $modx->getOption('editable', $scriptProperties, 'false');
+$aspectRatio = (float)$modx->getOption('aspectRatio', $scriptProperties, '0.4');
+$minTime = $modx->getOption('minTime', $scriptProperties, '6:00');
+$defaultView = $modx->getOption('defaultView', $scriptProperties, 'agendaWeek');
 
-$ajax_id = $modx->getOption('ajax_id',$scriptProperties,$modx->getOption('migxcalendar.ajax_id'));
+$ajax_id = $modx->getOption('ajax_id', $scriptProperties, $modx->getOption('migxcalendar.ajax_id'));
 $ajax_url = $modx->makeUrl($ajax_id);
 
-$load_jquery = $modx->getOption('load_jquery',$scriptProperties,'1');
-$load_jqueryui = $modx->getOption('load_jqueryui',$scriptProperties,'1');
-$load_bootstrap = $modx->getOption('load_bootstrap',$scriptProperties,'1');
-$packageName = $modx->getOption('packageName',$scriptProperties,'migxcalendars');
+$load_jquery = $modx->getOption('load_jquery', $scriptProperties, '1');
+$load_jqueryui = $modx->getOption('load_jqueryui', $scriptProperties, '1');
+$load_bootstrap = $modx->getOption('load_bootstrap', $scriptProperties, '1');
+$load_fullcalendar = $modx->getOption('load_fullcalendar', $scriptProperties, '1');
+
+$packageName = $modx->getOption('packageName', $scriptProperties, 'migxcalendars');
 
 $params = $_REQUEST;
 
-$categories = $modx->getOption('categories',$params,'');
-$date = $modx->getOption('date',$params,'');
+$categories = $modx->getOption('categories', $params, '');
+$date = $modx->getOption('date', $params, '');
+$view = $modx->getOption('view', $params, $defaultView);
 
-if (!empty($date)){
-    $defaultDate = "defaultDate : '". $date ."',";
+$scriptProperties['currentUrl'] = $modx->makeUrl($modx->resource->get('id'));
+
+if (!empty($date)) {
+    $defaultDate = "defaultDate : '" . $date . "',";
 }
 
-$extraOptionsTpl = $modx->getOption('extraOptionsTpl',$scriptProperties,'');
+$extraOptionsTpl = $modx->getOption('extraOptionsTpl', $scriptProperties, '');
 
-$extraOptions = !empty($extraOptionsTpl) ? ',' . $modx->getChunk($extraOptionsTpl) : '';
+$extraOptions = !empty($extraOptionsTpl) ? ',' . $modx->getChunk($extraOptionsTpl,$scriptProperties) : '';
 
 if ($modx->lexicon) {
     $modx->lexicon->load($packageName . ':default');
 }
 
-$script = "
+if (!empty($load_fullcalendar)) {
+    $script = "
 <script>
+var initialLoad = true;    
+var poppingState = false;
+
+window.onpopstate = function(event){
+    if (event.state.start){
+        if ( typeof migxcalController != 'undefined' ){
+            migxcalController.config.baseParams.original_request_uri = event.state.url; 
+        }
+        poppingState = true; //don't re-push state
+        $('#calendar').fullCalendar('gotoDate', event.state.start);
+        poppingState = true; //don't re-push state
+        $('#calendar').fullCalendar('changeView', event.state.viewMode);
+
+    }
+
+}
+
+if (window.history){
+    var windowHistory = window.history;
+    // symlink to method 'history.pushState'
+    var historyPushState = windowHistory.pushState;
+    // if the browser supports HTML5-History-API
+    var isSupportHistoryAPI = !!historyPushState; 
+}
+
+var migxcal_history = function(view,element,url){
+
+    // Prevent the code from running if there is no window.history object
+    if (!window.history) return;
+    var date_param = '?date=' + view.intervalStart.format('YYYY-MM-DD');
+    var view_param = '&view=' + view.name;
+    
+    url = isSupportHistoryAPI ? url : '';
+    url = url + date_param + view_param;
+
+        
+    if (initialLoad) { //Replace the current state to set up state variables.  URL should be identical
+        history.replaceState({ viewMode:view.name, start:view.intervalStart, url:url }, 'Edit Calendar', url);
+        if ( typeof migxcalController != 'undefined' ){
+            migxcalController.config.baseParams.original_request_uri = url; 
+        }
+        initialLoad = false;
+    }else{
+        if (!poppingState) { 
+            history.pushState({ viewMode:view.name, start:view.intervalStart, url:url }, 'Edit Calendar', url); 
+            if (typeof migxcalController != 'undefined'){
+                migxcalController.config.baseParams.original_request_uri = url; 
+            }
+        }else{
+            poppingState = false;
+    }
+}
+
+};
 
 	$(document).ready(function() {
 	    var migxcal_categories = {};
@@ -46,7 +106,7 @@ $script = "
             editable: {$editable},
             aspectRatio: {$aspectRatio},
             minTime: '{$minTime}',
-            defaultView:'{$defaultView}',
+            defaultView:'{$view}',
 			events: {
 				url: '{$ajax_url}',
                 data: function() {
@@ -80,23 +140,28 @@ $script = "
 </script>
 ";
 
-$modx->regClientCSS('assets/components/migxcalendars/js/fullcalendar/fullcalendar.css');
-$modx->regClientCSS('assets/components/migxcalendars/css/style.css');
-$modx->regClientStartupHTMLBlock('<link type="text/css" href="assets/components/migxcalendars/js/fullcalendar/fullcalendar.print.css" rel="stylesheet" media="print">');
-$modx->regClientScript('assets/components/migxcalendars/js/lib/moment.min.js');
-if (!empty($load_jquery)){
-    $modx->regClientScript('assets/components/migxcalendars/js/lib/jquery.min.js');    
+
+    $modx->regClientCSS('assets/components/migxcalendars/js/fullcalendar/fullcalendar.css');
+    $modx->regClientStartupHTMLBlock('<link type="text/css" href="assets/components/migxcalendars/js/fullcalendar/fullcalendar.print.css" rel="stylesheet" media="print">');
 }
-if (!empty($load_jquery)){
+
+if (!empty($load_jquery)) {
+    $modx->regClientScript('assets/components/migxcalendars/js/lib/jquery.min.js');
+}
+if (!empty($load_jquery)) {
     $modx->regClientScript('assets/components/migxcalendars/js/lib/jquery-ui.custom.min.js');
 }
 
-if (!empty($load_bootstrap)){
+if (!empty($load_bootstrap)) {
     $modx->regClientScript('assets/components/migxangular/bootstrap-3.0.0/js/bootstrap.min.js');
     $modx->regClientCSS('assets/components/migxangular/bootstrap-3.0.0/css/bootstrap.custom.css');
 }
+$modx->regClientCSS('assets/components/migxcalendars/css/style.css');
 
-
-$modx->regClientScript('assets/components/migxcalendars/js/fullcalendar/fullcalendar.min.js');
-$modx->regClientScript('assets/components/migxcalendars/js/fullcalendar/lang/de.js');
-$modx->regClientHTMLBlock($script);
+if (!empty($load_fullcalendar)) {
+    $modx->regClientScript('assets/components/migxcalendars/js/lib/moment.min.js');
+    $modx->regClientScript('assets/components/migxcalendars/js/fullcalendar/fullcalendar.min.js');
+    $modx->regClientScript('assets/components/migxcalendars/js/fullcalendar/lang/de.js');
+    $modx->regClientHTMLBlock($script);
+}
+$modx->regClientScript('assets/components/migxcalendars/js/history/history.min.js');
---
+++ core/components/migxcalendars/elements/snippets/migxcaldetailview.snippet.php
@@ -0,0 +1,22 @@
+$dateVarKey = $modx->getOption('dateVarKey', $scriptProperties, 'date_id');
+$date_id = $modx->getOption($dateVarKey, $_REQUEST, 0);
+
+$output = '';
+if (!empty($date_id)) {
+
+    $wheres = array();
+    $wheres[] = array('id'=>$date_id);
+    $wheres[] = array('Event.deleted' => 0, 'Event.published' => 1);
+
+    $scriptProperties['packageName'] = 'migxcalendars';
+    $scriptProperties['classname'] = 'migxCalendarDates';
+    $scriptProperties['joins'] = '[{"alias":"Event"},{"alias":"Category","classname":"migxCalendarCategories","on":"Category.id=Event.categoryid"}]';
+    $scriptProperties['where'] = $modx->toJson($wheres);
+
+    $output = $modx->runSnippet('migxLoopCollection', $scriptProperties);
+}
+
+$properties['load_fullcalendar'] = '0';
+$modx->runSnippet('migxcalCalendar', $properties);
+
+return $output;
